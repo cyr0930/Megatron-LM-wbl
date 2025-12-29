af2b97a843abcd6ccd689df6d9512feed2ff22a1
cyr0930 <line0930@gmail.com>
2025-12-28 20:14:47 -0800

    vllm 0.11.2

--- vllm_plugin/setup.py
+++ vllm_plugin/setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 shutil.copy("../bridge/configuration_wbl.py", "./wbl/")
 setup(
     name='wbl',
-    version='1.1.1',
+    version='1.2.0',
     packages=['wbl'],
     entry_points={
         'vllm.general_plugins': [
